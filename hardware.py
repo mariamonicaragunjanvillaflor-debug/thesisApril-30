f056006a37e27994b9170d0f42b0e839e5dbbb7a
mariamonicaragunjanvillaflor-debug <mariamonicaragunjanvillaflor@gmail.com>
2026-05-22 19:12:07 +0400

    Update hardware.py

--- hardware.py
+++ hardware.py
@@ -78,7 +78,7 @@ def read_temperature():
 # ADS1115 SETUP (FIXED - NO SMBUS)
 # =========================================================
 ads = ADS.ADS1115(i2c)
-ads.gain = 1
+ads.gain = 16
 ads.data_rate = 860
 
 chan = AnalogIn(ads, 0)
